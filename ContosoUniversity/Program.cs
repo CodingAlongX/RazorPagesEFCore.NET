f0f9ceed9bf466e4e2fb5fd98216db0c12fb730d
S. M. JAHANGIR <smjxpro@gmail.com>
2021-11-17 16:43:04 +0600

    Add the database exception filter

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddRazorPages();
 
 builder.Services.AddDbContext<SchoolContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("SchoolContextSQLite")));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
@@ -17,6 +18,11 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
