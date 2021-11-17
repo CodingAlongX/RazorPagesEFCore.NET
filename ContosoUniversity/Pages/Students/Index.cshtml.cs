7d1cd2bb5d4f8e2edae8f7a1945d1babe14aa602
S. M. JAHANGIR <smjxpro@gmail.com>
2021-11-17 23:42:42 +0600

    Add paging

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -14,23 +14,34 @@ namespace ContosoUniversity.Pages.Students
     public class IndexModel : PageModel
     {
         private readonly SchoolContext _context;
+        private readonly IConfiguration Configuration;
 
-        public IndexModel(SchoolContext context)
+        public IndexModel(SchoolContext context,IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
 
         public string NameSort { get; set; }
         public string DateSort { get; set; }
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
-        public IList<Student> Students { get; set; }
+        public PaginatedList<Student> Students { get; set; }
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        public async Task OnGetAsync(string sortOrder, string currentFilter,string searchString, int? pageIndex)
         {
+            CurrentSort = sortOrder;
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
 
+            if (searchString!=null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
             CurrentFilter = searchString;
             
             IQueryable<Student> studentsIQ = from s in _context.Students
@@ -54,7 +65,9 @@ namespace ContosoUniversity.Pages.Students
                     studentsIQ = studentsIQ.OrderBy(s => s.LastName);
                     break; 
             }
-            Students = await studentsIQ.AsNoTracking().ToListAsync();
+
+            var pageSize = Configuration.GetValue("PageSize", 4);
+            Students = await PaginatedList<Student>.CreateAsync(studentsIQ.AsNoTracking(),pageIndex??1,pageSize);
 
         }
     }
